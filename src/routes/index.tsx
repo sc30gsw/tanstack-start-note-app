b9b4bbc35ad27b3d76403fdb7773269aadd7a259
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-09-02 15:15:18 +0900

    🏗️ ルートコンポーネントをAppからHomeに変更

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,10 @@ import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
 
 export const Route = createFileRoute('/')({
-  component: App,
+  component: Home,
 })
 
-function App() {
+function Home() {
   return (
     <div className="text-center">
       <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-[calc(10px+2vmin)] text-white">
