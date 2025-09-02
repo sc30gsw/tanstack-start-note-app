2543ef2c9a04bedaba61d47a82791d9b891670fe
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-09-02 17:31:19 +0900

    ✨ Add get notes with Convex

--- src/routes/notes/index.tsx
+++ src/routes/notes/index.tsx
@@ -1,23 +1,13 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import { PlusIcon } from 'lucide-react'
+import { Suspense } from 'react'
+import Loader from '~/components/ui/loader'
 import { Button } from '~/components/ui/shadcn/button'
+import { NoteList } from '~/features/notes/components/note-list'
 
 export const Route = createFileRoute('/notes/')({
   component: NotesPage,
 })
-
-const notes = [
-  { title: 'Note 1', id: crypto.randomUUID() },
-  { title: 'Note 2', id: crypto.randomUUID() },
-  { title: 'Note 3', id: crypto.randomUUID() },
-  { title: 'Note 4', id: crypto.randomUUID() },
-  { title: 'Note 5', id: crypto.randomUUID() },
-  { title: 'Note 6', id: crypto.randomUUID() },
-  { title: 'Note 7', id: crypto.randomUUID() },
-  { title: 'Note 8', id: crypto.randomUUID() },
-  { title: 'Note 9', id: crypto.randomUUID() },
-  { title: 'Note 10', id: crypto.randomUUID() },
-] as const satisfies readonly Record<string, string>[]
 
 function NotesPage() {
   return (
@@ -30,16 +20,9 @@ function NotesPage() {
         </Button>
       </div>
       <div className="p-2">
-        {notes.map((note) => (
-          <Link
-            key={note.id}
-            to="/notes/$id"
-            params={{ id: note.id }}
-            className="transition-all duration-300 hover:text-blue-500"
-          >
-            <p>{note.title}</p>
-          </Link>
-        ))}
+        <Suspense fallback={<Loader />}>
+          <NoteList />
+        </Suspense>
       </div>
     </div>
   )
